31aff7821ac30d78b33aa223da23b7cc01cb96ec
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-06-05 12:49:48 -0700

    Consistent file paths for dev and prod

--- express-server/index.js
+++ express-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const compression = require('compression');
 
 const port = 3000;
-const app_folder = 'ng-dist';
+const app_folder = '../ng-frontend/dist/ng-frontend';
 
 const app = express();
 app.use(compression());
@@ -10,6 +10,8 @@ app.use(compression());
 
 app.get('*.*', express.static(app_folder, {maxAge: '1y'}));
 
-app.all('*', (req, res) => res.status(200).sendFile(`/`, {root: app_folder}));
+app.all('*', (req, res) =>
+    res.status(200).sendFile(`/`, {root: app_folder})
+);
 
-app.listen(port, () => console.log(`Example app listening at localhost:${port}`))
+app.listen(port, () => console.log(`Express server listening at localhost:${port}`))

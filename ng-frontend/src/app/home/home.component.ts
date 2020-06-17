329c2c3719ef51ce615389c3270b1898eaac4d43
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-06-17 11:57:59 -0700

    More content improvements

--- ng-frontend/src/app/home/home.component.ts
+++ ng-frontend/src/app/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  featuredHover = 'none';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateFeaturedHover(newTarget: string): void {
+    this.featuredHover = newTarget;
+  }
+
+  isHoveringOn(target: string): boolean {
+    return (this.featuredHover === target);
   }
 
 }

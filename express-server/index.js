74de99a7c4f26a6e7db19eb34a0ec89656cc026e
Nathaniel Warner <nathanielwarner77@gmail.com>
2024-02-17 12:27:31 -0500

    Allow changing port

--- express-server/index.js
+++ express-server/index.js
@@ -30,7 +30,7 @@ MongoClient.connect(dbConnUrl, {
         console.error(error);
     });
     
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app_folder = '../ng-frontend/dist/ng-frontend';
 
 const app = express();
